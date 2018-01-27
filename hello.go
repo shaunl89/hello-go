86062f108b9c4c102dc53ae3465ff0daccb92a09
Shaun Loh <loh.shaun89@gmail.com>
2018-01-27 12:47:01 +0800

    created dependency package stringutil to be used in hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/shaunloh89/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
 }
